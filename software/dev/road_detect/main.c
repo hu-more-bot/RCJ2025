e233850808c1fa37174a5e484a1fd888ee7a21cd
samthedev <samthedev32@gmail.com>
2025-03-28 19:17:20 +0100

    early road direction detection

--- software/dev/road_detect/main.c
+++ software/dev/road_detect/main.c
@@ -11,6 +11,7 @@
 
 #define SAMPLES 1000
 
+// Detection Parameters
 #define TRESHOLD_COLOR 100
 #define MIN_WIDTH 2 // (px)
 #define MAX_WIDTH 24 // (px)
@@ -42,30 +43,37 @@ int main() {
 
   // Process Frame
   struct Line line[2] = {};
+  float steering_value = 0;
 
   double start = now();
-  
+
   for (int i = 0; i < SAMPLES; i++) {
-    struct Line tmp;
     for (int y = 0; y < h; y++) {
       // x: pixel id in scanline
       unsigned char *scanline = &frame[y * w];
+      int mid = 0;
 
+      // find lines
+      struct Line tmp;
       for (int x = 0; x < w; x++) {
         if (scanline[x] > TRESHOLD_COLOR) {
           tmp.start = x, tmp.width = 0;
-            
+
           while (x < w && scanline[x] > TRESHOLD_COLOR)
             tmp.width++, x++;
 
           if (MIN_WIDTH <= tmp.width && tmp.width <= MAX_WIDTH) {
             const uint8_t i = line[0].width > line[1].width;
             if (line[i].width < tmp.width) memcpy(&line[i], &tmp, sizeof(struct Line));
+            mid = (line[0].start + line[1].start) / 2;
           }
         }
       }
 
-      if (line[0].width && line[1].width) {
+      if (i == 0 && line[0].width && line[1].width) {
+        memset(scanline + mid, 255, 10);
+        steering_value += (mid - w / 2.0f) * y;
+
         memset(scanline + line[0].start, 255, line[0].width);
         memset(scanline + line[1].start, 255, line[1].width);
 
@@ -76,7 +84,8 @@ int main() {
 
   double end = now();
 
-  printf("total: %f\n", (double)(end - start) / (double)SAMPLES);
+  printf("avg time elapsed: %fs\n", (double)(end - start) / (double)SAMPLES);
+  printf("steering value: %f\n", steering_value / h);
 
   stbi_write_bmp("out.bmp", w, h, 1, frame);
   free(frame);
