54cebcd0373c21384a37019b06c3a3308d0c2854
Miki <vargamiki53@gmail.com>
2025-04-02 20:14:52 +0200

    stuffer

--- software/raspberry/app.py
+++ software/raspberry/app.py
@@ -6,25 +6,38 @@ app.config['SECRET_KEY'] = 'secret!'
 
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
-    socketio.run(app)
 
 laps = 0
+race_going = False
 
+
+def addToLaps():
+    laps += 1
+    socketio.emit("update_laps", {"laps":laps})
+
+
+# ROUTES
 @app.route("/")
 def index():
     return render_template('index.html', laps=laps)
 
-@app.route("/getLaps")
-def getLaps():
-    global laps
-    laps += 1
-    socketio.emit('update_laps', {'laps': laps})  
-    return str(laps)
+@app.route("/dev")
+def dev():
+    return render_template('dev.html')
 
+#WEBSOCKET
 @socketio.on('connect')
 def handle_connect():
     emit('update_laps', {'laps': laps}) 
 
+@socketio.on("start")
+def start_race():
+    race_going = True
+    
+@socketio.on("end")
+def end_race():
+    race_going = False
+
+#INIT
 if __name__ == '__main__':
-    socketio.run(app) 
+    socketio.run(app, host="0.0.0", port=8000, debug=True)
