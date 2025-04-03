35706fd5aebfb15736fc48dadd67e20dde0bc313
Miki <vargamiki53@gmail.com>
2025-04-03 14:07:49 +0200

    lots

--- software/raspberry/app.py
+++ software/raspberry/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'  
@@ -8,35 +9,64 @@ socketio = SocketIO(app)
 
 
 laps = 0
-race_going = False
+race_start = 0 # miliseconds unix time since start
+race_end = 0
+lap_times = [] 
 
+def updateData():
+    socketio.emit("update_data", {"laps" : laps, "lap_times" : lap_times, "race_end" : race_end, "race_start" : race_start})
+    print("===== UPDATED DATA =====")
+
+def getMillis():
+    return round(time.time() * 1000)
 
 def addToLaps():
-    laps += 1
-    socketio.emit("update_laps", {"laps":laps})
-
+    global laps, lap_times
+    if(race_start > 0 and race_end == 0):
+        laps += 1
+        lap_times.append(getMillis())
+    updateData()
 
 # ROUTES
 @app.route("/")
 def index():
-    return render_template('index.html', laps=laps)
+    global laps, lap_times, race_end, race_start
+    return render_template('index.html', laps=laps, race_start=race_start, race_end=race_start,  lap_times=lap_times)
 
 @app.route("/dev")
 def dev():
-    return render_template('dev.html')
+    global laps, lap_times, race_end, race_start
+    return render_template('dev.html', laps=laps, race_start=race_start, race_end=race_end,  lap_times=lap_times)
 
 #WEBSOCKET
 @socketio.on('connect')
 def handle_connect():
-    emit('update_laps', {'laps': laps}) 
+    global laps, lap_times, race_end, race_start
+    emit('update_data', {"laps" : laps, "lap_times" : lap_times, "race_end" : race_end, "race_start" : race_start})
 
 @socketio.on("start")
 def start_race():
-    race_going = True
+    global race_start, race_end, lap_times, laps
+    if((race_start == 0 and race_end == 0)or(race_start > 0 and race_end>0)):
+        race_start = getMillis()
+        race_end = 0
+        lap_times = []
+        laps=0
+        updateData()
+        
     
 @socketio.on("end")
 def end_race():
-    race_going = False
+    global race_start, race_end
+    if(race_start > 0 and race_end == 0):
+        race_end= getMillis()
+        updateData()
+    
+    
+@socketio.on("DEV_ADD")
+def DEVADD():
+    addToLaps()
+    
 
 #INIT
 if __name__ == '__main__':
